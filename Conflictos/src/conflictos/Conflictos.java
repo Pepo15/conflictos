767f94bf84ec28452aea2cbaf6b3b5cbfa36f898
Pedro <VctorV@PC-VictorHerm>
2017-05-05 12:43:39 +0200

    Tercer Commit para probar conflicto: -Añado linea

--- Conflictos/src/conflictos/Conflictos.java
+++ Conflictos/src/conflictos/Conflictos.java
@@ -6,6 +6,7 @@ public class Conflictos {
     public static void main(String[] args) {
         System.out.println("Escrito por el pepillo....");
         System.out.println("Linea añadida por Pedro....");
+        System.out.println("Segunda linea añadida por Pedro....");
         
     }//Main
     
