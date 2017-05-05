cbec9a46f9b8c2198ca1f89741838b003a95d218
Pedro <>
2017-05-05 12:26:05 +0200

    Primer Commit: -Creo proyecto

---
+++ Conflictos/src/conflictos/Conflictos.java
@@ -0,0 +1,11 @@
+package conflictos;
+
+public class Conflictos {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Escrito por el pepillo....");
+        
+    }//Main
+    
+}//Class
